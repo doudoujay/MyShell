1518d26aba1fa3b341f79b6b99c5e41579a7bd96
Haoran Wang <wang2226@data.cs.purdue.edu>
2017-10-06 08:46:13 -0400

    Commit

--- command.hh
+++ command.hh
@@ -1,1 +1,32 @@
-"    cat_grep command.cc Command out\n"
+#ifndef command_h
+#define command_h
+
+#include "simpleCommand.hh"
+
+// Command Data Structure
+
+struct Command {
+	  int _numOfAvailableSimpleCommands;
+	  int _numOfSimpleCommands;
+	  SimpleCommand ** _simpleCommands;
+      char * _outFile;
+	  char * _inFile;
+	  char * _errFile;
+	  int _append; //>> flag
+	  int _background;
+	  int _inCounter;       //# of in
+	  int _outCounter;      //# of out
+
+	  void prompt();
+	  void print();
+	  void execute();
+	  void clear();
+	  int BuiltIn(int i);
+	  Command();
+	  void insertSimpleCommand( SimpleCommand * simpleCommand );
+	  
+      static Command _currentCommand;
+	  static SimpleCommand *_currentSimpleCommand;
+};
+
+#endif
