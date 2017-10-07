8dea143bc538ab93235349f4caa9236d04d5ef94
Haoran Wang <wang2226@data.cs.purdue.edu>
2017-10-06 20:43:31 -0400

    Commit

--- shell.cc
+++ shell.cc
@@ -10,6 +10,11 @@ extern "C" void ctrlC (int sig) {
 	Command::_currentCommand.prompt();
 }
 
+extern "C" void zombie(int sig) {
+	int pid = wait3(0, 0, NULL);
+	while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+
 int main() {
 	struct sigaction sigA1;
 	sigA1.sa_handler = ctrlC;
@@ -17,6 +22,17 @@ int main() {
 	sigA1.sa_flags = SA_RESTART;
 	int error = sigaction(SIGINT, &sigA1, NULL);
 	if(error){
+		perror("sigaction");
+		exit(-1);
+	}
+
+	//Zombie Process
+	struct sigaction sigA2;
+	sigA2.sa_handler = zombie;
+	sigemptyset(&sigA2.sa_mask);
+	sigA2.sa_flags = SA_RESTART;
+	error =  sigaction(SIGCHLD, &sigA2, NULL);
+	if (error) {
 		perror("sigaction");
 		exit(-1);
 	}
